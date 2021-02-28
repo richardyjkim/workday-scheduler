1ade893568c3a3549c93ec41bb20261863f4ee91
Richard <richardyjkim@gmail.com>
2021-02-27 22:08:57 -0800

    optimizing task, description, set,get localstorage and time interval

--- Develop/script.js
+++ Develop/script.js
@@ -5,54 +5,54 @@ $("#currentDay").append(currentDay);
 // task
 let tasks;
 
-let createTask = function(taskDescription, timeBlockId) {
-  let taskDescriptionBox = $(timeBlockId).find(".description");
-  taskDescriptionBox.text(taskDescription);
-};
-
-
 // save button
 $(".saveBtn").on("click",function(event) {
   let parent = $(this).parent();
-  let parent_id = parent[0].id;
-  let saveDescription = parent.children(".description")[0].value;
 
-  tasks[parent_id] = saveDescription;
+  let taskTextArea = parent.children(".description")[0];
+  let taskTextAreaId = taskTextArea.id;
+  let taskTextAreaValue = taskTextArea.value;
+
+  tasks[taskTextAreaId] = taskTextAreaValue;
   localStorage.setItem("tasks",JSON.stringify(tasks));
 });
 
 // load task
 let loadTasks = function() {
   tasks = JSON.parse(localStorage.getItem("tasks"));
-
   if (!tasks) {
     tasks = {};
   }
 
   $.each(tasks, function(hour, taskDescription) {
-    let timeBlockId = $("#" + hour + "");
-    createTask(taskDescription, timeBlockId);
+    let taskTextAreaId = $("#" + hour + "");
+    $(taskTextAreaId).text(taskDescription);
   });
 }; 
 
+let auditTask = function() {
 // color check
-let timeColor =$(".time-block");
-let now = parseInt(moment().format("H"));
+  let timeBlocks = $(".time-block");
+  let now = parseInt(moment().format("H"));
 
-$.each(timeColor, function (i,hour){
-  let hourId = parseInt($(this).attr("id"));
-  console.log(this);
-  if (hourId == now){
-    $(this).closest("description").addClass("present")
-    $(this).addClass("present");
-   
-  } else if (hourId < now) {
-    $(this).addClass("past")
 
-  } else if (hourId > now) {
-    $(this).addClass("future")
-  }
-});
+  $.each(timeBlocks, function (index, timeBlock) {
+    let timeBlockHour = parseInt($(timeBlock).attr("hour"));
+    if (timeBlockHour == now){
+      
+      $(timeBlock).addClass("present");
+    
+    } else if (timeBlockHour < now) {
+      $(timeBlock).addClass("past")
+
+    } else if (timeBlockHour > now) {
+      $(timeBlock).addClass("future")
+    }
+  });
+}
 
 loadTasks();
+auditTask();
 
+setInterval(function(){
+  auditTask();},3600000);
